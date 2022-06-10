41445700f33d691b45cc7ea4b86d92c2c274167e
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-06-10 11:55:02 +0300

    Replacing starter files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+
+import Application from '@/App'
+
+import { store } from '@/store'
+import theme from '@/theme'
+import GlobalStyles from '@/globalStyles'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Application />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root'),
+)
